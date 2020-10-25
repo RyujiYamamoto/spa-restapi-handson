f74b02cdb3ed1420a0407f7491e6b5066e6df138
RyujiYamamoto <yamamoto.ryuji.dev@gmail.com>
2020-10-25 16:15:25 +0900

    REST APIクライアントの生成

---
+++ todo-app/frontend/src/backend/BackendService.ts
@@ -0,0 +1,56 @@
+import {
+  Configuration,
+  TodosApi,
+  Middleware,
+  UsersApi
+} from './generated-rest-client';
+
+const requestLogger: Middleware = {
+  pre: async (context) => {
+    console.log(`>> ${context.init.method} ${context.url}`, context.init);
+  },
+  post: async (context) => {
+    console.log(`<< ${context.response.status} ${context.url}`, context.response);
+  }
+}
+
+const configuration = new Configuration({
+  middleware: [requestLogger]
+});
+
+const todosApi = new TodosApi(configuration);
+
+const usersApi = new UsersApi(configuration);
+
+const signup = async (userName: string, password: string) => {
+  return usersApi.signup({ inlineObject2: { userName, password } });
+}
+
+const login = async (userName: string, password: string) => {
+  return usersApi.login({ inlineObject3: { userName, password } });
+}
+
+const logout = async () => {
+  return usersApi.logout();
+}
+
+const getTodos = async () => {
+  return todosApi.getTodos();
+}
+
+const postTodo = async (text: string) => {
+  return todosApi.postTodo({ inlineObject: { text } });
+}
+
+const putTodo = async (todoId: number, completed: boolean) => {
+  return todosApi.putTodo({ todoId, inlineObject1: { completed } });
+}
+
+export const BackendService = {
+  signup,
+  login,
+  logout,
+  getTodos,
+  postTodo,
+  putTodo
+};
