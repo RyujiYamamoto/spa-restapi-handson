8308329dae51e7c387add740a0d24ebe7c38be54
RyujiYamamoto <yamamoto.ryuji.dev@gmail.com>
2020-10-25 19:14:04 +0900

    ToDoの一覧表示>REST APIの呼び出しとstateの更新

--- todo-app/frontend/src/components/TodoBoard.tsx
+++ todo-app/frontend/src/components/TodoBoard.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { BackendService } from '../backend/BackendService';
 import './TodoBoard.css';
 import { TodoFilter } from './TodoFilter';
 import { TodoForm } from './TodoForm';
@@ -11,11 +12,11 @@ type Todo = {
 };
 
 export const TodoBoard: React.FC = () => {
-  const [todos] = useState<Todo[]>([
-    { id: 2001, text: '洗い物をする', completed: true },
-    { id: 2002, text: '洗濯物を干す', completed: false },
-    { id: 2003, text: '買い物へ行く', completed: false },
-  ]);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    BackendService.getTodos().then((response) => setTodos(response));
+  }, []);
 
   return (
     <div className='TodoBoard_content'>
