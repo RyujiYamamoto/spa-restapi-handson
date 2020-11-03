caa836aff626d7dec96e261ea5cb172e235eca92
RyujiYamamoto <yamamoto.ryuji.dev@gmail.com>
2020-11-03 12:55:58 +0900

    ToDoを登録する処理の実装 : REST APIの作成

--- todo-app/backend/src/main/java/com/example/todo/api/TodosAction.java
+++ todo-app/backend/src/main/java/com/example/todo/api/TodosAction.java
@@ -4,8 +4,11 @@ import com.example.todo.domain.*;
 
 import com.example.todo.application.TodoService;
 import nablarch.core.repository.di.config.externalize.annotation.SystemRepositoryComponent;
-
+import nablarch.core.validation.ee.ValidatorUtil;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -29,6 +32,26 @@ public class TodosAction {
     List<Todo> todos = todoService.list(userId);
     return todos.stream().map(todo -> new TodoResponse(todo.id(), todo.text(), todo.status()))
         .collect(Collectors.toList());
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public TodoResponse post(PostRequest requestBody) {
+    ValidatorUtil.validate(requestBody);
+
+    UserId userId = new UserId("1002");
+    TodoText text = new TodoText(requestBody.text);
+
+    Todo todo = todoService.addTodo(userId, text);
+
+    return new TodoResponse(todo.id(), todo.text(), todo.status());
+  }
+
+  public static class PostRequest {
+    @NotNull
+    // TODO: 他に必要な精査は何だろう？
+    public String text;
   }
 
   public static class TodoResponse {
