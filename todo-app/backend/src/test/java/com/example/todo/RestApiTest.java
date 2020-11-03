906e6aa53dae3abd5fdc39f036bff8ba73ecf9a1
RyujiYamamoto <yamamoto.ryuji.dev@gmail.com>
2020-11-03 13:01:09 +0900

    ToDoを登録する処理の実装 : REST APIのテスト

--- todo-app/backend/src/test/java/com/example/todo/RestApiTest.java
+++ todo-app/backend/src/test/java/com/example/todo/RestApiTest.java
@@ -4,13 +4,15 @@ import com.example.openapi.OpenApiValidator;
 import nablarch.fw.web.HttpResponse;
 import nablarch.fw.web.RestMockHttpRequest;
 import nablarch.test.core.http.SimpleRestTestSupport;
-
+import org.hamcrest.Matchers;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openapi4j.core.validation.ValidationException;
 
 import java.nio.file.Paths;
-
+import java.util.Collections;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -18,8 +20,8 @@ import static org.hamcrest.Matchers.hasSize;
 
 public class RestApiTest extends SimpleRestTestSupport {
 
-  public static OpenApiValidator openApiValidator = new OpenApiValidator(
-      Paths.get("rest-api-specification/openapi.yaml"));
+  public static OpenApiValidator openApiValidator =
+      new OpenApiValidator(Paths.get("rest-api-specification/openapi.yaml"));
 
   @Test
   public void RESTAPIでToDo一覧が取得できる() throws ValidationException {
@@ -41,5 +43,29 @@ public class RestApiTest extends SimpleRestTestSupport {
     assertThat(responseBody, hasJsonPath("$[1].completed", equalTo(false)));
 
     openApiValidator.validate("getTodos", request, response);
+  }
+
+  @Test
+  public void RESTAPIでToDoを登録できる() throws Exception {
+    RestMockHttpRequest request = post("/api/todos")
+        .setHeader("Content-Type", MediaType.APPLICATION_JSON).setBody(Map.of("text", "テストする"));
+    HttpResponse response = sendRequest(request);
+
+    assertStatusCode("ToDoの登録", HttpResponse.Status.OK, response);
+
+    assertThat(response.getBodyString(), hasJsonPath("$.id", Matchers.notNullValue()));
+    assertThat(response.getBodyString(), hasJsonPath("$.text", equalTo("テストする")));
+    assertThat(response.getBodyString(), hasJsonPath("$.completed", equalTo(false)));
+
+    openApiValidator.validate("postTodo", request, response);
+  }
+
+  @Test
+  public void ToDo登録時にtext項目が無い場合_登録に失敗して400になる() {
+    RestMockHttpRequest request = post("/api/todos")
+        .setHeader("Content-Type", MediaType.APPLICATION_JSON).setBody(Collections.emptyMap());
+    HttpResponse response = sendRequest(request);
+
+    assertStatusCode("ToDoの登録", HttpResponse.Status.BAD_REQUEST, response);
   }
 }
