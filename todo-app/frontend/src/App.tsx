35342af3f859dd45837f8728743b3ee3512ae509
RyujiYamamoto <yamamoto.ryuji.dev@gmail.com>
2020-11-17 22:34:32 +0900

    ユーザーコンテクストの作成

--- todo-app/frontend/src/App.tsx
+++ todo-app/frontend/src/App.tsx
@@ -6,26 +6,29 @@ import { NavigationHeader } from './components/NavigationHeader';
 import { Signup } from './components/Signup';
 import { TodoBoard } from './components/TodoBoard';
 import { Welcome } from './components/Welcome';
+import { UserContextProvider } from './contexts/UserContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavigationHeader />
-      <Switch>
-        <Route exact path='/board'>
-          <TodoBoard />
-        </Route>
-        <Route exact path='/signup'>
-          <Signup />
-        </Route>
-        <Route exact path='/login'>
-          <Login />
-        </Route>
-        <Route exact path='/'>
-          <Welcome />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <UserContextProvider>
+      <BrowserRouter>
+        <NavigationHeader />
+        <Switch>
+          <Route exact path='/board'>
+            <TodoBoard />
+          </Route>
+          <Route exact path='/signup'>
+            <Signup />
+          </Route>
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+          <Route exact path='/'>
+            <Welcome />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </UserContextProvider>
   );
 }
 
