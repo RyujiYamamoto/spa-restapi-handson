8285a2bf2a49bb705e5467ac38fe3dd76123e07b
RyujiYamamoto <yamamoto.ryuji.dev@gmail.com>
2020-11-17 23:11:18 +0900

    サインアップ: 入力値のバリデーション

--- todo-app/frontend/src/components/Signup.tsx
+++ todo-app/frontend/src/components/Signup.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useUserContext } from '../contexts/UserContext';
 import { useInput } from '../hooks/useInput';
+import { stringField, useValidation } from '../validation';
 import './Signup.css';
+
+type ValidationFields = {
+  userName: string;
+  password: string;
+};
 
 export const Signup: React.FC = () => {
   const history = useHistory();
@@ -16,20 +22,27 @@ export const Signup: React.FC = () => {
     history.push('/');
   };
 
+  const { error, handleSubmit } = useValidation<ValidationFields>({
+    userName: stringField().required('名前を入力してください'),
+    password: stringField().required('パスワードを入力してください').minLength(4, 'パスワードは4桁以上入力してください'),
+  });
+
   return (
     <div className='Signup_content'>
       <div className='Signup_box'>
         <div className='Signup_title'>
           <h1>ユーザー登録</h1>
         </div>
-        <form className='Signup_form' onSubmit={signup}>
+        <form className='Signup_form' onSubmit={handleSubmit({ userName, password }, signup)}>
           <div className='Signup_item'>
             <div className='Signup_label'>名前</div>
             <input type='text' {...userNameAttributes} />
+            <div className='error'>{error.userName}</div>
           </div>
           <div className='Signup_item'>
             <div className='Signup_label'>パスワード</div>
             <input type='password' {...passwordAttributes} />
+            <div className='error'>{error.password}</div>
           </div>
           <div className='Signup_buttonGroup'>
             <button className='Signup_button'>登録する</button>
